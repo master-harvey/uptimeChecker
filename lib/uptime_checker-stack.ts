e57fb65edefe35b83e03b9fd554bc21b7c17b0a5
Master__Harvey <ianpadraicharvey@gmail.com>
2023-02-23 04:00:24 -0700

    added function name, function inputs via env vars, and destination topic

--- lib/uptime_checker-stack.ts
+++ lib/uptime_checker-stack.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import {
   aws_sns as sns,
-  aws_sns_subscriptions as subscripitons,
+  aws_lambda_destinations as destinations,
   aws_lambda as lambda,
   aws_events as events,
   aws_events_targets as targets
@@ -12,32 +12,30 @@ export class UptimeCheckerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    if (this.node.tryGetContext('timeout') == "" || this.node.tryGetContext('URL') == "" || this.node.tryGetContext('topic') == "") { throw ("URL, topic, and timeout context variables required") }
+    if (this.node.tryGetContext('URL') == "" || this.node.tryGetContext('topic') == "") { throw ("URL and topic context variables required") }
 
-    const timeout = Number(this.node.tryGetContext('timeout'))
     const topicArn = this.node.tryGetContext('topic')
     const URL = this.node.tryGetContext('URL')
 
+    const topic = sns.Topic.fromTopicArn(this, "topic", topicArn)
+
     const requestFunction = new lambda.Function(this, 'Singleton', {
+      functionName: "uptimeChecker",
       code: lambda.Code.fromInline(`#Check URL uptime, return success if available, false and notify SNS if not
       from botocore.vendored import requests
-      response = requests.get("https://httpbin.org/get", timeout=${timeout})
-      return { 
-        "statusCode": response.status_code, "body": { "status": response.status_code },
-        "headers": { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*" }
-      }
+      def handler():
+        response = requests.get(environ['URL'], timeout=20)
+        return { 
+          "statusCode": response.status_code, "body": { "status": response.status_code, "URL": environ['URL'] },
+          "headers": { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*" }
+        }
       `),
+      environment: { URL },
       handler: 'index.function_name',
-      timeout: Duration.seconds(timeout),
+      timeout: Duration.seconds(20),
       runtime: lambda.Runtime.PYTHON_3_9,
+      onFailure: new destinations.SnsDestination(topic)
     });
-
-    const topic = sns.Topic.fromTopicArn(this, "topic", topicArn)
-    topic.addSubscription(new subscripitons.LambdaSubscription(requestFunction, {
-      filterPolicy: {
-        status: sns.SubscriptionFilter.numericFilter({lessThan: 200, greaterThan: 300}),
-      }
-    }))
 
     // Run every day at 4am UTC https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
     const rule = new events.Rule(this, 'Rule', { schedule: events.Schedule.expression('cron(0 4 ? * * *)') });
